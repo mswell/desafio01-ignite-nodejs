375df534df0e15086c13dc391112332140b462da
Wellington Moraes <wellpunk@gmail.com>
2021-09-21 10:34:07 -0300

    Delete todos

--- src/index.js
+++ src/index.js
@@ -75,11 +75,11 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
   const { user } = request;
   const { id } = request.params;
-
+ 
   const todoIndex = user.todos.findIndex(
     (todo) => todo.id === id,
-  );
-
+  )
+  
   if (todoIndex < 0) {
     return response.status(404).json({ error: 'Todo not found.' });
   }
@@ -113,7 +113,22 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex(
+    (todo) => todo.id === id,
+  );
+
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: 'Todo not found.' });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).send();
+  
 });
 
 module.exports = app;
